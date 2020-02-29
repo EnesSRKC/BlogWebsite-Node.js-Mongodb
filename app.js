0f20bb7d1142a72a0fdc8da0b20d69e1bf5b1c6c
Enes Sirkecioğlu <enessrkc237@gmail.com>
2020-02-29 12:01:35 +0300

    Update hostname

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const methodOverride = require('method-override');
 const app = express();
-const port = 3000;
-const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+const hostname = 'https://heroku-blogsite.herokuapp.com';
 
 mongoose.connect('mongodb://127.0.0.1/nodeblog_db', {
   useNewUrlParser: true,
